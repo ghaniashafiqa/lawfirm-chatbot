016a4230483c6b6789ede6a921f5a19d9789f157
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-07-01 11:37:12 +0700

    update: dockerfile

--- backend/run.py
+++ backend/run.py
@@ -5,7 +5,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
 
 # backend/run.py [LOCAL]
 # from app import create_app
