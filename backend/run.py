fd6409e17b9865e3e04562e3392510d2fdb5f442
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-07-01 11:08:40 +0700

    prod config

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,16 @@
-# backend/run.py
+# backend/run.py [PROD]
+
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run()
+
+# backend/run.py [LOCAL]
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
