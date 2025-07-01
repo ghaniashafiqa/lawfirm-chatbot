3ec571be04191da96ded78dd37c07421746b6403
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-07-01 11:09:36 +0700

    update: packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './assets/tailwind.css'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+app.use(Toast, { timeout: 3000, position: 'top-right' })
