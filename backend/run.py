13ce42a3581c8068690324f6b3d8dfb93dec42c2
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-07-03 10:39:56 +0700

    fix: migrations, move alembic.ini to be roots

--- backend/run.py
+++ backend/run.py
@@ -8,11 +8,13 @@ import os
 
 app = create_app()
 
-# Only run alembic upgrade if in production (Railway)
+# Only run alembic migration in Railway or production
 if os.environ.get("RAILWAY_ENVIRONMENT"):
     with app.app_context():
         print("🔧 Running Alembic migrations...")
-        alembic_cfg = Config("alembic.ini")
+        cfg_path = os.path.join(os.path.dirname(__file__), 'alembic.ini')
+        print("📄 Alembic config path:", cfg_path)
+        alembic_cfg = Config(cfg_path)
         command.upgrade(alembic_cfg, "head")
 
 if __name__ == "__main__":
